c2d0584dce938d9c41278489070779ff4dc9a74b
Ferponce <fernyeponce@gmail.com>
2020-05-02 19:57:55 -0300

    Inicial

---
+++ LPPA/Ejercicios/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicios.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ConceptosBasicos1()
+        {
+            return View();
+        }
+        public ActionResult ConceptosBasicos2()
+        {
+            return View();
+        }
+        public ActionResult ConceptosBasicos3()
+        {
+            return View();
+        }
+        public ActionResult Enlaces()
+        {
+            return View();
+        }
+        public ActionResult Listas()
+        {
+            return View();
+        }
+        public ActionResult Tablas1()
+        {
+            return View();
+        }
+
+        public ActionResult Tablas2()
+        {
+            return View();
+        }
+        public ActionResult Formularios1()
+        {
+            return View();
+        }
+    }
+}
