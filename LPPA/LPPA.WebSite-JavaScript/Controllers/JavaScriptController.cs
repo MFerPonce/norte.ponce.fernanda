52c108a7253eb92d5c974fc2fb78f920d11fa570
Ferponce <fernyeponce@gmail.com>
2020-05-31 23:24:47 -0300

    Ejercicio JavaScript

---
+++ LPPA/LPPA.WebSite-JavaScript/Controllers/JavaScriptController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LPPA.WebSite_JavaScript.Controllers
+{
+    public class JavaScriptController : Controller
+    {
+        // GET: JavaScript
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Variables()
+        {
+            return View();
+        }
+
+        public ActionResult Condicionales()
+        {
+            return View();
+        }
+
+        public ActionResult Funciones()
+        {
+            return View();
+        }
+    }
+}
---
+++ LPPA/LPPA.WebSite-JavaScript/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LPPA.WebSite_JavaScript.MvcApplication" Language="C#" %>
